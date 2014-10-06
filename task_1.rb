31037a84c9fe39211af4588f0e7f61123f9b669a
Taylor Horn <Taylor@Sharon-Horns-MacBook-Pro.local>
2014-10-06 12:50:44 -0400

    Did all the questions, but I couldn't figure out how to do number 6 correctly.

--- task_1.rb
+++ task_1.rb
@@ -14,11 +14,10 @@ puts "Hi #{name}, your age is #{age}"
 
 # 3. Ask if the person is old enough to drive. If the answer is yes then say "Have a nice Drive"
 #    if the answer is no, then say "Oh sorry!"
-
 puts "Are you old enough to drive?:"
 answer = get_input
 if answer == "yes"
-  puts "Have a nice Drive!"
+  puts "Have a nice drive!"
 else
   puts "Oh sorry!"
 end
@@ -26,18 +25,58 @@ end
 # 4. Get the user's bank balance. If less than zero, say "Oh you are in trouble"
 #    If greater than zero but less than 100 say "You can buy McDonalds. Yay"
 #    If greater than 1000 say "Wow you are rich!!!" (conversion)
+puts "Enter your bank account balance:"
+bal = gets.chomp.to_i
+if bal >0 and bal <100
+	puts "Yay! You can buy McDonalds!"
+elsif bal >1000
+	puts "Wow you are rich!!!"
+end
+
+
 
 # 5. Ask the user how happy they are from 1 to 10. Print out "Happy" between 1 and 10 times
 #    based on what they said. (conversion)
+puts "How happy are you on a scale of 1-10?:"
+happy = gets.chomp.to_i 
+happy.times do 
+	puts "Happy!"
+end
 
-# 6. Ask the user how happy they are from 1 to 10. Print out "Happy" between 1 and 10 times
+
+
+# 6. Ask the user how happy they are from 1 to 10. Print out "Happy" between 1 and 10 times	
 #    based on what they said. ON THE EVEN NUMBERS PRINT "Very Happy" instead (every even number)
+def even(n)
+
+remainder = n % 2
+if remainder == 0
+	puts "Very Happy"
+else
+	puts "Happy"
+end
+end
+puts "How happy are you on a scale of 1-10?:"
+hpy = gets.chomp.to_i
+hpy.times do
+	even(hpy)
+ end
+
+
+
 
 # 7. Ask the user for their 3 favorite fruits. (Ask 3 separate times). Create an array from those
 #    answers and say "I love bananas" for each one. (bananas obviously changes based off the fruit
 #    they chose)
-
-
-
+fruits = Array.new
+puts "What is your favorite fruit?"
+fruits << gets.chomp
+puts "What is your second favorite?:"
+fruits << gets.chomp
+puts "What is your third favorite?:"
+fruits << gets.chomp
+puts "I love "+fruits[0]
+puts "I love "+fruits[1]
+puts "I love "+fruits[2]
 
 
